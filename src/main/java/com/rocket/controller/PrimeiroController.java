9393b5aa26cd89c8c0099da302acd3a7c0949b8e
Victor <victor.ciechovicz@merx.tech>
2025-10-21 16:36:38 -0300

    feat:passando parametro

--- src/main/java/com/rocket/controller/PrimeiroController.java
+++ src/main/java/com/rocket/controller/PrimeiroController.java
@@ -1,6 +1,7 @@
 package com.rocket.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/primeiroController")
 public class PrimeiroController {
 
-    @GetMapping("/primeiroMetodo")
-    public String primeiroMetodo() {
-        return "Hello World!";
+    @GetMapping("/primeiroMetodo/{id}")
+    public String primeiroMetodo(@PathVariable String id) {
+        return "o paramtro é " + id;
     }
 }
