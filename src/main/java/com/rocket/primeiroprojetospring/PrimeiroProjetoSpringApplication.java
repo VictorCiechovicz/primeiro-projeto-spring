a7363b61eadf9156339ba533aeabb63cf3d752d3
Victor <victor.ciechovicz@merx.tech>
2025-10-21 08:57:41 -0300

    first commit

---
+++ src/main/java/com/rocket/primeiroprojetospring/PrimeiroProjetoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.rocket.primeiroprojetospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroProjetoSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrimeiroProjetoSpringApplication.class, args);
+    }
+
+}
