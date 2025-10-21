0045cc34cdf713b8b5dfa9ffe439951091017b2b
Victor <victor.ciechovicz@merx.tech>
2025-10-21 17:20:51 -0300

    feat:studo de metodos

--- src/main/java/com/rocket/controller/PrimeiroController.java
+++ src/main/java/com/rocket/controller/PrimeiroController.java
@@ -1,9 +1,11 @@
 package com.rocket.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/primeiroController")
@@ -11,6 +13,50 @@ public class PrimeiroController {
 
     @GetMapping("/primeiroMetodo/{id}")
     public String primeiroMetodo(@PathVariable String id) {
-        return "o paramtro é " + id;
+        return "o parametro é " + id;
     }
+
+    @GetMapping("/metotoQueryParms")
+    public String metotoQueryParms(@RequestParam String id) {
+        return "o parametro é " + id;
+    }
+
+    //forma de passar varios parametros e mapear eles atraves do Map
+    @GetMapping("/metotoListQueryParms2")
+    public String metotoListQueryParms(@RequestParam Map<String, String> allParams) {
+        return "o parametro é " + allParams.entrySet();
+    }
+
+
+    //recurso do spring para criar uma classe sem setters e getters
+    record Usuario(String name, String email) {
+    }
+
+    @PostMapping("/metodoBodyParams")
+    public String metodoBodyParams(@RequestBody Usuario usuario) {
+        return "o parametro é " + usuario.name;
+    }
+
+
+    @PostMapping("/metodoHeaderParams")
+    public String metodoHeaderParams(@RequestHeader("name") String name) {
+        return "o parametro é " + name;
+    }
+
+    @PostMapping("/metodoListHeaderParams")
+    public String metodoLisHeaderParams(@RequestHeader() Map<String, String> headers) {
+        return "o parametro é " + headers.entrySet();
+    }
+
+    @GetMapping("/metodoResponseEntity/{id}")
+    public ResponseEntity<Object> metodoResponsyEntity(@PathVariable Long id) {
+        Usuario user = new Usuario("victor", "victor@html.com");
+
+        if (id > 5) {
+            return ResponseEntity.ok(user);
+        }
+        return ResponseEntity.status(400).body("Error");
+    }
+
+
 }
